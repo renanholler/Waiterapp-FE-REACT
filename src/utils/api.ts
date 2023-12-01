f714e3e3b0896399db0ad4e3d6a35dfeccca15d9
Renan Holler <rrholler@gmail.com>
2023-12-01 13:01:16 -0300

    fixes

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://192.168.3.48:3001'
-  // baseURL: 'http://10.90.35.124:3001'
+  baseURL: 'http://192.168.3.103:3001'
 });
